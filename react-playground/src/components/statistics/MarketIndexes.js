e591d2a74816ddcff21a809a130b04d868eaa7ae
Abhijit Jadhav <Abhijit.Jadhav@nice.com>
2024-05-22 13:46:37 +0530

    Crypto Dashboard

---
+++ react-playground/src/components/statistics/MarketIndexes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// Font Awesome Icon
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faChartColumn as ChartColumnIcon } from '@fortawesome/free-solid-svg-icons';
+library.add(ChartColumnIcon);
+
+const MarketIndexes = () => {
+
+  // TODO - Fetch Indexes - use axios to fetch data
+  // using endpoint - https://api.coingecko.com/api/v3/indexes
+  const fetchIndexes = () => {  };
+
+  return (
+    <></>
+  );
+};
+
+export default MarketIndexes;
